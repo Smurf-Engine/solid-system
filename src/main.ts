2810220808e00b19335560b4a5cceaeafc42029a
siddastic <siddhantgaurav2003@gmail.com>
2023-09-15 12:41:21 +0530

    game loop

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
-class Game{
-  
+import { Level } from "./level";
+import { Settings } from "./settings";
+
+class Game {
+  previousTime = Date.now();
+  level = new Level();
+
+  constructor() {
+    this.run();
+  }
+
+  run = () => {
+      let newTime = Date.now();
+      Settings.add('dt',(newTime - this.previousTime) / 1000);
+      this.previousTime = newTime;
+
+      this.level.run();
+
+      requestAnimationFrame(this.run);
+  }
 }
+
+onload = () => {
+  new Game()
+};
